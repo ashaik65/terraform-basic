9caca60890f141190ec650c1a7fad6f114b234bb
ashaik65 <anisshaikh1439@gmail.com>
2023-08-08 16:30:20 +0530

    adding the files

---
+++ 01-Terraform-basic/terraform-files/ec2-instance.tf
@@ -0,0 +1,22 @@
+# Terraform Settings Block
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.11.0"
+      #version = "~> 5.10" # Optional but recommended in production
+    }
+  }
+}
+
+# Provider Block
+provider "aws" {
+  profile = "default" # AWS Credentials Profile configured on your local desktop terminal  $HOME/.aws/credentials
+  region  = "us-east-1"
+}
+
+# Resource Block
+resource "aws_instance" "ec2demo" {
+  ami           = "ami-0261755bbcb8c4a84" # Amazon Linux in us-east-1, update as per your region
+  instance_type = "t2.micro"
+}
